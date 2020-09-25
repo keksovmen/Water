179fb8e5d8598fdbb55b2342ca4714cb015816b4
Keksovmen <Keksovmen@yandex.ru>
2020-09-25 16:27:21 +0300

    Made find string test

--- Buffer/FixedBufferTest.cpp
+++ Buffer/FixedBufferTest.cpp
@@ -7,6 +7,8 @@
 
 void checkLength(int divider);
 
+void findString(const char* origin, const char* search, bool expected);
+
 int main()
 {
 	using namespace std;
@@ -14,6 +16,10 @@ int main()
 	checkLength(1);
 	checkLength(2);
 	checkLength(4);
+	
+	findString("123456789", "567", true);
+	findString("123456789", "111", false);
+	findString("asd dsa qwe fgh", " ds", true);
 }
 
 void checkLength(int divider){
@@ -24,4 +30,22 @@ void checkLength(int divider){
 		buffer+= 'f';
 	}
 	assert(strlen(buffer.begin()) == buffer.getLength());
+}
+
+void findString(const char* origin, const char* search, bool expected){
+	FixedBuffer<512> buffer;
+	
+	int size = strlen(origin);
+	assert(size < buffer.getSize());
+	
+	for(int i = 0; i < size; i++){
+		buffer += origin[i];
+	}
+	
+	char* result = strstr(buffer.begin(), search);
+	if(result){
+		assert(expected == true);
+	}else{
+		assert(expected == false);
+	}
 }
