3b770db3dbd4e445182083b5150ab8df4a61460b
Keksovmen <Keksovmen@yandex.ru>
2021-02-04 18:29:14 +0300

    Enchanced a bit DB

--- SQL/DB_Creation.sql
+++ SQL/DB_Creation.sql
@@ -10,8 +10,8 @@ grant all on arduino to 'java'@'localhost';
 
 create table arduino (
 	arduino_id INT NOT NULL,
-	imei BIGINT NOT NULL,
-	is_active BOOL NOT NULL,
+	imei varchar(20) NOT NULL,
+	is_active tinyint(1) NOT NULL,
 	last_time_update DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	CONSTRAINT pk_arduino PRIMARY KEY (arduino_id)
 	
@@ -20,10 +20,10 @@ create table arduino (
 create table sensor_entries (
 	arduino_id INT NOT NULL,
 	time DATETIME NOT NULL,
-	upload_time DATETIME DEFAULT CURRENT_TIMESTAMP,
 	temp_up FLOAT NOT NULL,
 	temp_down FLOAT NOT NULL,
-	CONSTRAINT pk_sensor PRIMARY KEY (arduino_id),
+	upload_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT pk_sensor PRIMARY KEY (arduino_id, time),
 	CONSTRAINT fk_sensor FOREIGN KEY (arduino_id) REFERENCES arduino (arduino_id)
 	
 );
